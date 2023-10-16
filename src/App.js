505746c13d0ae36adc4f32cc66f82278c5c0500c
avishmehta <avishmehta68@gmail.com>
2023-10-16 23:23:55 +0530

    front end added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from './component/Form';
+import Home from './component/Home';
+import Form1_l2 from './component/Form1_l2';
+import Form2_l2 from './component/Form2_l2';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+ <BrowserRouter>
+ <Navbar></Navbar>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/form" element={<Form />} />
+      <Route exact path="/form1_l2" element={<Form1_l2 />} />
+      <Route exact path="/form2_l2" element={<Form2_l2 />}/>
+       {/* Add the '/form' route */}
+    </Routes>
+  <Footer></Footer>
+    </BrowserRouter>
+    </>
   );
 }
 
